4d4d94606178642229394b1dbe7d5b20edc315a9
IEdiong@Mint <ediongi6@hotmail.com>
2021-09-15 22:22:40 +0100

    reset 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
-import { AuthorsComponent } from './authors/authors.component';
 import { HistoryComponent } from './history/history.component';
-import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    AuthorsComponent,
     HistoryComponent,
-    CardComponent
   ],
   imports: [
     BrowserModule,
